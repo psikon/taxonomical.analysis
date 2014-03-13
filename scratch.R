a01c282f39c5e3513acb27ff860b2621f08302aa
psikon86 <psikon86@gmail.com>
2014-03-13 13:54:57 +0100

    change metadata SampleID

--- scratch.R
+++ scratch.R
@@ -3,46 +3,64 @@ load.project()
 Sys.setenv("PKG_CXXFLAGS" = "-std=c++11")
 registerDoParallel(cores=20)
 
+# load all databases and generate a biom file with it
 path <- generateBiomFile("bacterial")
+# init the phyloseq object
 phylo <- generatePhyloseq("bacterial")
-class(phylo@sam_data$SampleName)
+
+#"genus"
+
 # subset the phyloseq object based on taxonomy expressions
-euk <- subset_taxa(phylo, superkingdom == "k__Eukaryota")
-fun <- subset_taxa(phylo, phylum == "p__Opisthokonta")
-vir <- subset_taxa(phylo, superkingdom == "k__Viruses")
+fungi <- subset_taxa(phylo, superkingdom == "k__Eukaryota")
+# only artefacts
+#vir <- subset_taxa(phylo, superkingdom == "k__Viruses")
 bakteria <- subset_taxa(phylo, superkingdom == 'k__Bacteria')
-bak <- prune_taxa(taxa_sums(bak) > 2, bak)
-sum(count(get_taxa(phylo,"80"))$x*count(get_taxa(phylo,"80"))$freq)
-sum(count(get_taxa(phylo,"62"))$x*count(get_taxa(phylo,"62"))$freq)
-bak
-
+# remove contamination
 bakteria <- remove_taxa(bakteria,"2")
-bak
-otu_table(bakteria)
-taxon(1029823)
-taxon(964)
-taxon(85698)
-taxon(672)
-taxon(662)
-taxon(645)
-taxon(346)  
-taxon(33917)
-taxon(1773)
-taxon(231049)
-taxon(1236)
-taxon(1224)
-taxon(1158459)
-taxon(1110389)
-taxon(38293)
-pdf("graphs/bacteria.pdf")
-    plot_overview_bar(phyloseq=bakteria, level="family",seperator="Environment")
-dev.off()
+bakteria
+# Overview Phylum
+phylum <- tax_glom(bakteria,taxrank="phylum")
+phylum <- prune_taxa(taxa_sums(phylum) > 10, phylum)
+plot_overview_bar(phylum,level="phylum",seperator="Environment")
+otu_phylum <- otu_table(phylum)
+nrow(otu_phylum)
+
+# Overview Class
+class <- tax_glom(bakteria, taxrank="class")
+otu_class <- otu_table(class)
+nrow(otu_class)
+class <- prune_taxa(taxa_sums(class) > 10, class)
+plot_overview_bar(family,level="class",seperator="Environment")
+
+# Overview Order
+order <- tax_glom(bakteria, taxrank="order")
+otu_order <- otu_table(order)
+nrow(otu_order)
+order <- prune_taxa(taxa_sums(order) > 10, order)
+plot_overview_bar(family,level="order",seperator="Environment")
+
+# Overview family
+family <- tax_glom(bakteria,taxrank="family")
+otu_family <- otu_table(family)git pull
+
+nrow(otu_family)
+family <- prune_taxa(taxa_sums(family) > 10, family)
+plot_overview_bar(family,level="family",seperator="Environment")
+plot_bar(phylum, facet_grid=~Environment)
+# Overview genus
+genus <- tax_glom(bakteria, taxrank="genus")
+otu_genus <- otu_table(genus)
+nrow(otu_genus)
+genus <- prune_taxa(taxa_sums(genus) > 10, genus)
+plot_overview_bar(genus,level="genus",seperator="Environment")
+
+# Overview species
+species <- tax_glom(bakteria, taxrank="species")
+otu_table(species)
+nrow(otu_table(species))
+species <- prune_taxa(taxa_sums(species) > 5, species)
+plot_overview_bar(species,level="species",seperator="Environment")
 
 
 
-plot_heatmap(bak)
-pdf("graphs/initial_heat.pdf")
-plot_heatmap(bak, low = "#000033", high = "#FF3300")
-dev.off()
-sample60
 
